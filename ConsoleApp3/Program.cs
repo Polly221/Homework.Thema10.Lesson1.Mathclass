fba6cd0c493acb5a1e5799f1201e7b3c8bd26061
Polinka <Polinka@DESKTOP-MHBJDTO>
2023-05-12 20:50:11 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите радиус окружности:");
+double r = double.Parse(Console.ReadLine());
+double L = 2 * Math.PI * r;
+double S = Math.PI * r * r;
+Console.WriteLine($"Длина окружности = {L:F2}");
+Console.WriteLine($"Площадь окружности =  {S:F2}");
